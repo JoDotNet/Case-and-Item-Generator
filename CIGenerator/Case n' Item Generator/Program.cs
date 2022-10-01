b6b6e4a170cd80f7099feca8c9c5a23e62e27085
XxJOxX25 <johnoscarbhv@gmail.com>
2022-10-01 16:48:28 +0200

    .

---
+++ CIGenerator/Case n' Item Generator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case_n__Item_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
